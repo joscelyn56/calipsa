239832dc29d157000954ad109a47311830137b71
Okwu Joscelyn Kinikachi <ojk564@gmail.com>
2021-07-18 21:58:21 +0100

    Update auth token check to use Bearer token

--- src/utils/authJwt.js
+++ src/utils/authJwt.js
@@ -2,25 +2,31 @@ const jwt = require('jsonwebtoken')
 const config = require('../config/config.js')
 
 verifyToken = (req, res, next) => {
-    let token = req.headers['authorization']
-
-    if (!token) {
-        return res.status(200).send({
-            error: true,
-            msg: 'Please login to continue using the service.'
-        })
-    }
-
-    jwt.verify(token, config.secret, (err, decoded) => {
-        if (err) {
-            return res.status(200).send({
-                error: true,
-                msg: 'Authentication Failed.'
-            })
-        }
-        req.user = decoded.id
-        next()
-    })
+	const token = req.headers['authorization']
+	
+	if (!token) {
+		return res.status(403).send({
+			message: 'Please login to continue using the service.'
+		})
+	}
+	
+	if (!token.includes("Bearer")) {
+		return res.status(401).send({
+			message: 'Authentication Failed.'
+		})
+	}
+	
+	const loginToken = token.split(" ")[1];
+	
+	jwt.verify(loginToken, config.secret, (err, decoded) => {
+		if (err) {
+			return res.status(401).send({
+				message: 'Authentication Failed.'
+			})
+		}
+		req.user = decoded.id
+		next()
+	})
 }
 
 const authJwt = {}
